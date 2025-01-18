83b9a315bf7c32fc477a47c4ed57305e88e625d0
hatef khodkar <hatef.khodkar@hotmail.com>
2025-01-18 10:21:30 +0330

    feat: improve add expense form

--- libs/finance/ui/expenses-registration-forms/src/lib/add-expense-reactive-form/add-expense-reactive-form.component.ts
+++ libs/finance/ui/expenses-registration-forms/src/lib/add-expense-reactive-form/add-expense-reactive-form.component.ts
@@ -1,8 +1,27 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  QueryList,
+  signal,
+  ViewChildren,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddExpenseReactive } from '../add-expense.interface';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { maxWordCountValidator } from '@bt-libs/shared/util/form-validators';
+import { debounceTime, of, Subject, switchMap, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'bt-libs-ui-add-expense-reactive-form',
@@ -11,13 +30,18 @@ import { maxWordCountValidator } from '@bt-libs/shared/util/form-validators';
   styleUrl: './add-expense-reactive-form.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AddExpenseReactiveFormComponent {
+export class AddExpenseReactiveFormComponent implements OnInit, OnDestroy {
+
+  destroy$ = new Subject<void>();
+  disabledAddTagButton = signal(true);
+  @ViewChildren('tags') tags!: QueryList<ElementRef<HTMLInputElement>>;
+
   @Input()
   public set expenseToAdd(value: AddExpenseReactive) {
     this.addExpenseForm.patchValue(value);
 
     this.addExpenseForm.controls.tags.clear();
-    value.tags?.forEach(tag => {
+    value.tags?.forEach((tag) => {
       this.addExpenseForm.controls.tags.push(new FormControl(tag));
     });
   }
@@ -25,19 +49,42 @@ export class AddExpenseReactiveFormComponent {
   @Output() addExpense = new EventEmitter<AddExpenseReactive>();
 
   addExpenseForm = new FormGroup({
-    description: new FormControl('',Validators.compose([Validators.required, maxWordCountValidator(4)])),
+    description: new FormControl(
+      '',
+      Validators.compose([Validators.required, maxWordCountValidator(4)])
+    ),
     amount: new FormGroup({
-      amountExclVat: new FormControl<number | null>(null, [Validators.required]),
-      vatPercentage: new FormControl<number | null>(null, [Validators.required, Validators.min(0), Validators.max(100)]),
+      amountExclVat: new FormControl<number | null>(null, [
+        Validators.required,
+      ]),
+      vatPercentage: new FormControl<number | null>(null, [
+        Validators.required,
+        Validators.min(0),
+        Validators.max(100),
+      ]),
     }),
-    date: new FormControl([''], [Validators.required]),
-    tags: new FormArray([
-      new FormControl(''),
-    ])
+    date: new FormControl(new Date().toISOString().split('T')[0], [Validators.required]),
+    tags: new FormArray([new FormControl('', Validators.required)]),
   });
 
+  ngOnInit(): void {
+    this.addExpenseForm.valueChanges
+    .pipe(
+      takeUntil(this.destroy$),
+      debounceTime(300),
+      switchMap(formData =>  of(!!formData.tags && formData.tags.some(c => !c))),
+    )
+    .subscribe( (isAddTagsDisable) => this.disabledAddTagButton.set(isAddTagsDisable))
+  }
+
+
   addTag() {
-    this.addExpenseForm.controls.tags.insert(0, new FormControl(''));
+    if(this.addExpenseForm.controls.tags.controls.some(c => c.valid)){
+      this.addExpenseForm.controls.tags.insert(0, new FormControl('', Validators.required));
+      setTimeout(() => {
+        this.tags.first.nativeElement.focus()
+      })
+    }
   }
 
   removeTag(index: number) {
@@ -45,7 +92,15 @@ export class AddExpenseReactiveFormComponent {
   }
 
   onSubmit() {
-    this.addExpense.emit(structuredClone(this.addExpenseForm.value as AddExpenseReactive));
+    this.addExpense.emit(
+      structuredClone(this.addExpenseForm.value as AddExpenseReactive)
+    );
+    console.log(this.addExpenseForm.value);
     this.addExpenseForm.reset();
   }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
 }
